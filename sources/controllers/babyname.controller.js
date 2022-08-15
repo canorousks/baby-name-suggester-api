979d60d1a0d3b565df683dca02cf2b5d305504bd
kanhaiya-cp <107399447+kanhaiya-cp@users.noreply.github.com>
2022-08-15 15:12:43 +0530

    console log changes

--- sources/controllers/babyname.controller.js
+++ sources/controllers/babyname.controller.js
@@ -9,33 +9,18 @@ const babyNameController = {
                 message: "Content can not be empty!"
             });
         }
-        console.log(req.body);
         
         var query = {};
         for(const [key,value] of Object.entries(req.body)){
             if(value)query[key] = value;
         }
         console.log(query);
-        // query = {};
-        // function find (name, query, cb) {
-        //     mongoose.connection.db.collection(name, function (err, collection) {
-        //        collection.find(query).toArray(cb);
-        //    });
-        // }
-        // find("babyNames",query,(err,data)=>{
-        //     if(err){
-        //         console.log(err);
-        //     }
-        //     else{
-        //         console.log(data);
-        //     }
-        // })
         babyNameModel.find(query,(err,data)=>{
             if(err){
                 console.log(err);
             }
             else{
-                console.log(data);
+                // console.log(data);
                 res.send(data);
             }
         });
